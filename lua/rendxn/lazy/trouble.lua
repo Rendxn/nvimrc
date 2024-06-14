30cc8c3548c7a94d690f0595a387327feb89c8ba
Santiago Rendón Giraldo <52801967+Rendxn@users.noreply.github.com>
2024-06-14 16:28:40 -0500

    trouble keymaps

--- lua/rendxn/lazy/trouble.lua
+++ lua/rendxn/lazy/trouble.lua
@@ -4,10 +4,11 @@ return {
 	cmd = "Trouble",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
 	keys = {
-		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open/close trouble list" },
+		{ "<leader>xq", "<cmd>Trouble close<CR>", desc = "Close trouble list" },
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open/close diagnostics trouble list" },
 		{ "<leader>xw", "<cmd>Trouble workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
 		{ "<leader>xd", "<cmd>Trouble document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-		{ "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
+		{ "<leader>xf", "<cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
 		{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
 		{ "<leader>xt", "<cmd>Trouble todo filter = {tag = {TODO,FIX,FIXME}}<CR>", desc = "Open todos in trouble" },
 	},
