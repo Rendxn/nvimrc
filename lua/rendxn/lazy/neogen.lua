bde244ea73f319054e989c84b908ca3de5320214
Santiago Rendón Giraldo <52801967+Rendxn@users.noreply.github.com>
2024-06-14 17:14:54 -0500

    neogen

--- lua/rendxn/lazy/neogen.lua
+++ lua/rendxn/lazy/neogen.lua
@@ -7,9 +7,9 @@ return {
 			require("neogen").setup()
 
 			local opts = { noremap = true, silent = true }
-			vim.keymap.set("n", "<Leader>nf", "<cmd>Neogen func", opts)
-			vim.keymap.set("n", "<Leader>nt", "<cmd>Neogen type", opts)
-			vim.keymap.set("n", "<Leader>nc", "<cmd>Neogen class", opts)
+			vim.keymap.set("n", "<Leader>nf", "<cmd>Neogen func<CR>", opts)
+			vim.keymap.set("n", "<Leader>nt", "<cmd>Neogen type<CR>", opts)
+			vim.keymap.set("n", "<Leader>nc", "<cmd>Neogen class<CR>", opts)
 		end,
 	},
 }
