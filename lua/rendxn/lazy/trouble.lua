16703cdb5290fb0cf8e97d2c43e8cf1b25844541
Santiago Rendón Giraldo <52801967+Rendxn@users.noreply.github.com>
2024-06-17 01:38:37 -0500

    fix trouble document

--- lua/rendxn/lazy/trouble.lua
+++ lua/rendxn/lazy/trouble.lua
@@ -6,8 +6,11 @@ return {
 	keys = {
 		{ "<leader>xq", "<cmd>Trouble close<CR>", desc = "Close trouble list" },
 		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open/close diagnostics trouble list" },
-		{ "<leader>xw", "<cmd>Trouble workspace_diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>xd", "<cmd>Trouble document_diagnostics toggle<CR>", desc = "Open trouble document diagnostics" },
+		{
+			"<leader>xd",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+			desc = "Open trouble document diagnostics",
+		},
 		{ "<leader>xf", "<cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
 		{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
 		{
