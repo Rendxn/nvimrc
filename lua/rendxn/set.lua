c3204c6fdf790668f0dffe935ed9c853315253e7
Santiago Rendón Giraldo <52801967+Rendxn@users.noreply.github.com>
2024-06-19 17:32:31 -0500

    set wrap and textwidth

--- lua/rendxn/set.lua
+++ lua/rendxn/set.lua
@@ -10,8 +10,6 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
@@ -20,7 +18,7 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 -- vim.opt.termguicolors = true
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
@@ -28,4 +26,21 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
+vim.opt.wrap = true
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	pattern = { "*.md" },
+	callback = function()
+		vim.opt.colorcolumn = "80"
+		vim.opt.textwidth = 80
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+	pattern = { "*.md" },
+	callback = function()
+		vim.opt.colorcolumn = "120"
+		vim.opt.textwidth = 120
+	end,
+})
