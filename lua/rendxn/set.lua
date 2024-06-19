6ce4b30a2a494cc32281f3b3a47151c430f64a16
Santiago Rendón Giraldo <52801967+Rendxn@users.noreply.github.com>
2024-06-19 17:48:13 -0500

    remove wrap

--- lua/rendxn/set.lua
+++ lua/rendxn/set.lua
@@ -26,21 +26,5 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "120"
-vim.opt.wrap = true
-
-vim.api.nvim_create_autocmd("BufWinEnter", {
-	pattern = { "*.md" },
-	callback = function()
-		vim.opt.colorcolumn = "80"
-		vim.opt.textwidth = 80
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
-	pattern = { "*.md" },
-	callback = function()
-		vim.opt.colorcolumn = "120"
-		vim.opt.textwidth = 120
-	end,
-})
+vim.opt.colorcolumn = "100"
+vim.opt.wrap = false
