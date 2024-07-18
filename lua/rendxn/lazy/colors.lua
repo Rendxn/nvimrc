48a3ae7743a8180678e9702f342e359c36f51d51
Santiago Rendón <santirg2000@gmail.com>
2024-07-18 13:50:35 -0500

    tokyonight

--- lua/rendxn/lazy/colors.lua
+++ lua/rendxn/lazy/colors.lua
@@ -1,5 +1,5 @@
 return {
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+	{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
 }
-
