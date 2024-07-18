5c5531cd727705cfcd329f601b3ff2d20462c5a9
Santiago Rendón <santirg2000@gmail.com>
2024-07-18 14:00:13 -0500

    feat(neotree): follow current file

--- lua/rendxn/lazy/neotree.lua
+++ lua/rendxn/lazy/neotree.lua
@@ -1,14 +1,18 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-      require("neo-tree").setup({})
-      vim.keymap.set('n', '<C-E>', '<Cmd>Neotree toggle<CR>')
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = { enabled = true },
+			},
+		})
+		vim.keymap.set("n", "<C-E>", "<Cmd>Neotree toggle<CR>")
+	end,
 }
