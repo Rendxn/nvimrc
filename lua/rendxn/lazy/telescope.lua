4e290cf70137c780c956b212bda293afe82df38b
Santiago Rendon <santirg2000@gmail.com>
2024-07-19 11:43:24 -0500

    feat(telescope): change to live_grep and keymap fixed strings version

--- lua/rendxn/lazy/telescope.lua
+++ lua/rendxn/lazy/telescope.lua
@@ -1,24 +1,29 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-        require('telescope').setup({})
+    require('telescope').setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+    local conf = require('telescope.config').values
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "Find files" })
+    vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = "Find git files" })
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.live_grep({ search = word })
+    end, { desc = "Search nearest word in workspace with regex" })
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.live_grep({ search = word })
+    end, { desc = "Search selected word in workspace with regex" })
+    vim.keymap.set('n', '<leader>ps', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.live_grep({
+        search = word,
+        vimgrep_arguments = table.insert(conf.vimgrep_arguments, '--fixed-strings'),
+      })
+    end, { desc = "Search text in workspace" })
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Search help tags" })
+  end
 }
-
