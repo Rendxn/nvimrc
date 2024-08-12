133a4393035811ad6bf587ecb0c7fe8bc7e42542
Santiago Rendón <santirg2000@gmail.com>
2024-08-12 17:39:40 -0500

    feat: indent guides and statuscol for prettier fold column

--- lua/rendxn/lazy/ufo.lua
+++ lua/rendxn/lazy/ufo.lua
@@ -1,10 +1,26 @@
 return {
 	"kevinhwang91/nvim-ufo",
 	events = "VeryLazy",
-	dependencies = { "kevinhwang91/promise-async" },
+	dependencies = {
+		"kevinhwang91/promise-async",
+		{
+			"luukvbaal/statuscol.nvim",
+			config = function()
+				local builtin = require("statuscol.builtin")
+				require("statuscol").setup({
+					relculright = true,
+					segments = {
+						{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+						{ text = { "%s" }, click = "v:lua.ScSa" },
+						{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+					},
+				})
+			end,
+		},
+	},
 	init = function()
 		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-		vim.o.foldcolumn = "auto:9"
+		vim.o.foldcolumn = "1"
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
