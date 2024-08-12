e837e42c5689ef76b5e93fc0e1185a1c377530cc
Santiago Rendón <santirg2000@gmail.com>
2024-08-12 16:44:42 -0500

    fix(conform): configure autoformat toggle

--- lua/rendxn/lazy/conform.lua
+++ lua/rendxn/lazy/conform.lua
@@ -18,10 +18,13 @@ return {
 					lua = { "stylua" },
 					go = { "goimports", "gofumpt", "golines" },
 				},
-				format_on_save = {
-					lsp_format = "fallback",
-					timeout_ms = 500,
-				},
+				format_on_save = function(bufnr)
+					-- Disable with a global or buffer-local variable
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+						return
+					end
+					return { timeout_ms = 500, lsp_format = "fallback" }
+				end,
 			})
 
 			vim.keymap.set({ "n", "v" }, "<leader>f", function()
