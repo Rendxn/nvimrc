fcaaa923404c78b035fe17509037c748c2458edb
Santiago Rendón <santirg2000@gmail.com>
2025-08-01 10:56:17 -0500

    fix: update telescope config

--- lua/rendxn/config/init.lua
+++ lua/rendxn/config/init.lua
@@ -1,3 +1,3 @@
-require("rendxn.config.lazy_init")
 require("rendxn.config.options")
 require("rendxn.config.autocmds")
+require("rendxn.config.lazy_init")
--- lua/rendxn/plugins/telescope.lua
+++ lua/rendxn/plugins/telescope.lua
@@ -14,12 +14,12 @@ return {
       local separator = package.config:sub(1, 1)
 
       require("telescope").setup({
-        pickers = {
-          find_files = {
-            file_ignore_patterns = { ".git" .. separator },
-            hidden = true, -- show hidden/dotfiles
-          },
-        },
+        -- pickers = {
+        -- find_files = {
+        --   file_ignore_patterns = { ".git" .. separator, "node_modules" },
+        --   hidden = true, -- show hidden/dotfiles
+        -- },
+        -- },
       })
 
       require("telescope").load_extension("ui-select")
