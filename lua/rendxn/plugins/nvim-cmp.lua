8103564ca879f5b6c4bde5305476244cab294c09
Santiago Rendón <santirg2000@gmail.com>
2025-08-01 14:23:58 -0500

    fix(nvim-cmp): cmdline completion

--- lua/rendxn/plugins/nvim-cmp.lua
+++ lua/rendxn/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-cmdline",
     {
       "L3MON4D3/LuaSnip",
       -- follow latest release.
@@ -47,8 +48,31 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
+      }, {
         { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
+      }),
+    })
+
+    -- `/` cmdline setup.
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          },
+        },
       }),
     })
   end,
