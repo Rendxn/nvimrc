563497de6e2aa8c3b5258f644b46c12ced797855
Santiago Rendon <santiago.x.rendon.-nd@disney.com>
2025-08-13 13:29:29 -0500

    feat(diffview): change mergetool layout to mixed (vscode-like)

--- lua/rendxn/plugins/diffview.lua
+++ lua/rendxn/plugins/diffview.lua
@@ -2,4 +2,11 @@ return {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    view = {
+      merge_tool = {
+        layout = "diff3_mixed",
+      },
+    },
+  },
 }
