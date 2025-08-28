bd61d1c5fb41f6f2abd996eadfc81fd618ec4b6c
Santiago Rendon <santiago.x.rendon.-nd@disney.com>
2025-08-28 10:33:24 -0500

    opts(treesitter): cap multiline context

--- lua/rendxn/plugins/neotest.lua
+++ lua/rendxn/plugins/neotest.lua
@@ -3,6 +3,7 @@ return {
     "nvim-neotest/neotest",
     -- pinning to this commit because I'm getting
     -- tests not found with newer versions
+    -- see: https://github.com/nvim-neotest/neotest/issues/531
     commit = "52fca67",
     dependencies = {
       "nvim-neotest/nvim-nio",
--- lua/rendxn/plugins/treesitter-context.lua
+++ lua/rendxn/plugins/treesitter-context.lua
@@ -1,4 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    multiline_threshold = 5, -- Maximum number of lines to show for a single context
+  },
 }
